1493f5adcbba15d037dcbd6ba2038a0020fe3916
mohammed-alobaidi270 <80877307+mohammed-alobaidi270@users.noreply.github.com>
2023-11-30 09:10:56 +0300

    displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,19 +1,30 @@
-import { Card, CardBody, GridItem, Heading, Image } from '@chakra-ui/react'
-import React from 'react'
-import { Game } from '../hooks/useGames';
+import {
+  Card,
+  CardBody,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { Game } from "../hooks/useGames";
+import PlatformIIconList from "./PlatformIIconList";
 
-interface GameCardProps{
-    game:Game;
+interface GameCardProps {
+  game: Game;
 }
-const GameCard = ({game}:GameCardProps) => {
+const GameCard = ({ game }: GameCardProps) => {
   return (
-   <Card borderRadius={10} overflow='hidden'>
-    <Image src={game.background_image}/>
-    <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-    </CardBody>
-   </Card>
-  )
-}
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
+    </Card>
+  );
+};
 
-export default GameCard
+export default GameCard;
---
+++ src/components/PlatformIIconList.tsx
@@ -0,0 +1,39 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+interface PlatformIIconListProps {
+  platforms: Platform[];
+}
+const PlatformIIconList = ({ platforms }: PlatformIIconListProps) => {
+  const IconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={IconMap[platform.slug]} color='gray.500'/>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIIconList;
