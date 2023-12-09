5b79cc0eab78a1022c07581d1ca0965f26e4d3dd
mohammed-alobaidi270 <80877307+mohammed-alobaidi270@users.noreply.github.com>
2023-12-09 18:16:18 +0300

    Show a skeleton while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,21 +1,72 @@
-import { HStack, List, ListItem, Image,Text } from "@chakra-ui/react";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Text,
+  Spinner,
+  Skeleton,
+  SkeletonText,
+  CardBody,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image_url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  const skeletons = [1, 2, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  if (error) {
+    return null;
+  }
+  //   if (isLoading) {
+  //     return (
+  //       <List>
+  //         {skeletons.map((skeleton) => (
+  //           <ListItem key={skeleton} paddingY="5px">
+  //             <HStack>
+  //               <Skeleton boxSize="32px" borderRadius={8} />
+  //               <SkeletonText fontSize="lg" />
+  //             </HStack>
+  //           </ListItem>
+  //         ))}
+  //       </List>
+  //     );
+  //   } else {
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack >
-            <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-            <Text fontSize='lg'>{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && (
+        <List>
+          {skeletons.map((skeleton) => (
+            <ListItem key={"skeleton" + skeleton} paddingY="5px">
+              <HStack>
+                <Skeleton boxSize="32px" borderRadius={8} />
+                  <SkeletonText fontSize="lg">
+                  Massively Multiplayer
+                  </SkeletonText>
+              </HStack>
+            </ListItem>
+          ))}
+        </List>
+      )}
+      {
+        <List>
+          {data.map((genre) => (
+            <ListItem key={genre.id} paddingY="5px">
+              <HStack>
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Text fontSize="lg">{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          ))}
+        </List>
+      }
+    </>
   );
 };
+// };
 
 export default GenreList;
